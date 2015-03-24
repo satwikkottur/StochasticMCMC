fcfb29c53b38652ce62da16b91f995733b919026
satwikkottur <satwik27@gmail.com>
2015-03-24 15:25:49 -0400

    Added L1 normalization

---
+++ src/GaussianMeanL1/generatePlots.m
@@ -0,0 +1,38 @@
+function generatePlots(data, mcmcSamples, truePDF, priorPDF, initGuess)
+    % Generating some plots of true sampling and posterior sampling
+
+    % Plotting the true samples
+    figure(1); hold all
+        plot(data(:, 1), data(:, 2), 'x')
+        % True mean
+        plot(truePDF.mean(1), truePDF.mean(2), 'o');
+    hold off
+
+    %%%%%%%%%%%%%%%%%%% Checking the mean %%%%%%%%%%%%%%%%%%%
+    noData = size(data, 1);
+    %postMean = (priorPDF.precision + noData * truePDF.precision) \ ...
+    %            (priorPDF.precision * priorPDF.mean' +  ...
+    %                truePDF.precision * sum(data)');
+    mle = mean(data)';
+    
+    fprintf('True mean: (%f %f)\n', truePDF.mean(1), truePDF.mean(2));
+    %fprintf('Post mean: (%f %f)\n', postMean(1), postMean(2));
+    fprintf('MLE mean : (%f %f)\n', mle(1), mle(2));
+    %%%%%%%%%%%%%%%%%%% Checking the variance %%%%%%%%%%%%%%%%%%%
+    %postVariance = inv(priorPDF.precision + noData * truePDF.precision);
+    %fprintf('Post variance = \n'); disp(postVariance)
+    
+    shifted = bsxfun(@minus, data, mean(data));
+    sampleVariance = 1/noData * (shifted' * shifted);
+    fprintf('Sample variance = \n'); disp(sampleVariance)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Plotting the MCMC samples and posterior
+    figure(2); hold all
+        plot(mcmcSamples(:, 1), mcmcSamples(:, 2), 'x')
+        %plot(postMean(1), postMean(2), 'o')
+        plot(mle(1), mle(2), '*');
+        plot(initGuess(1), initGuess(2), 's')
+    hold off
+
+end
+
---
+++ src/GaussianMeanL1/runScript.m
@@ -0,0 +1,50 @@
+% Default script to be run for test/debugging purposes
+close all
+display('Running Bayesian Lasso');
+tic;
+
+noDims = 2; % Number of dimensions
+noSamples = 1000; % Number of samples
+
+% Create the dataset
+[data, truePDF] = generateDataset(noDims, noSamples); 
+
+% Running HMC, select with gradient or stochastic gradient
+prob = @likelihood;
+gradProb = @gradLikelihood;
+%gradProb = @stocGradLikelihood;
+
+% Initializing the options (manually done checking the code in hmc)
+options = -1 * ones(18, 1);
+options(9) = 0; % false
+options(14) = 10000; % Run for 50000 iterations
+options(15) = 50; % burn in
+options(7) = 10; % Number of leap steps
+options(1) = 0; % Display 
+options(18) = 0.0001; %step size
+
+% Relatively good prior
+priorPDF = struct('lambda', 0.0001);
+            
+% Generating multiple samples
+noMCMC = 1;
+mcmcSamples = zeros(noMCMC, noDims);
+
+% Infomation for selecting the batches
+batchSize = 20;
+batchSelect = 1;  % 1 for random and 2 for linear
+batchInfo = struct('size', batchSize, 'select', batchSelect);
+
+for i = 1:noMCMC
+    initGuess = rand(1, noDims);
+    [samples, energies, diagn] = hmc(prob, initGuess, options, gradProb, ...
+                                      data, priorPDF, truePDF, options(18), batchInfo);
+    % Selecting the batches at 'random' or in a 'linear' way
+    
+    mcmcSamples = samples(1:100:size(samples, 1), :);
+end
+
+% Verify samples
+generatePlots(data, mcmcSamples, truePDF, priorPDF, initGuess);
+%generateTrajectory(data, samples, truePDF);                                                
+toc
