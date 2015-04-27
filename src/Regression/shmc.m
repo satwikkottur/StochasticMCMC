8c8f01e5b941ab558774b430d019a54c0adb3711
satwikkottur <satwik27@gmail.com>
2015-04-27 13:20:44 -0400

    Regression/ shmc.m half done

--- src/Regression/shmc.m
+++ src/Regression/shmc.m
@@ -1,81 +1,6 @@
-function [samples, energies, diagn] = shmc(f, x, options, gradf, ...,
-                                data, priorPDF, batchSize, fisher, varargs)
+function [samples, energies] = shmc(f, x, options, gradf, ...,
+                                XData, yData, lambda, batchSize, fisher, varargs)
 %HMC	Hybrid Monte Carlo sampling.
-%
-%	Description
-%	SAMPLES = HMC(F, X, OPTIONS, GRADF) uses a  hybrid Monte Carlo
-%	algorithm to sample from the distribution P ~ EXP(-F), where F is the
-%	first argument to HMC. The Markov chain starts at the point X, and
-%	the function GRADF is the gradient of the `energy' function F.
-%
-%	HMC(F, X, OPTIONS, GRADF, P1, P2, ...) allows additional arguments to
-%	be passed to F() and GRADF().
-%
-%	[SAMPLES, ENERGIES, DIAGN] = HMC(F, X, OPTIONS, GRADF) also returns a
-%	log of the energy values (i.e. negative log probabilities) for the
-%	samples in ENERGIES and DIAGN, a structure containing diagnostic
-%	information (position, momentum and acceptance threshold) for each
-%	step of the chain in DIAGN.POS, DIAGN.MOM and DIAGN.ACC respectively.
-%	All candidate states (including rejected ones) are stored in
-%	DIAGN.POS.
-%
-%	[SAMPLES, ENERGIES, DIAGN] = HMC(F, X, OPTIONS, GRADF) also returns
-%	the ENERGIES (i.e. negative log probabilities) corresponding to the
-%	samples.  The DIAGN structure contains three fields:
-%
-%	POS the position vectors of the dynamic process.
-%
-%	MOM the momentum vectors of the dynamic process.
-%
-%	ACC the acceptance thresholds.
-%
-%	S = HMC('STATE') returns a state structure that contains the state of
-%	the two random number generators RAND and RANDN and the momentum of
-%	the dynamic process.  These are contained in fields  randstate,
-%	randnstate and mom respectively.  The momentum state is only used for
-%	a persistent momentum update.
-%
-%	HMC('STATE', S) resets the state to S.  If S is an integer, then it
-%	is passed to RAND and RANDN and the momentum variable is randomised.
-%	If S is a structure returned by HMC('STATE') then it resets the
-%	generator to exactly the same state.
-%
-%	The optional parameters in the OPTIONS vector have the following
-%	interpretations.
-%
-%	OPTIONS(1) is set to 1 to display the energy values and rejection
-%	threshold at each step of the Markov chain. If the value is 2, then
-%	the position vectors at each step are also displayed.
-%
-%	OPTIONS(5) is set to 1 if momentum persistence is used; default 0,
-%	for complete replacement of momentum variables.
-%
-%	OPTIONS(7) defines the trajectory length (i.e. the number of leap-
-%	frog steps at each iteration).  Minimum value 1.
-%
-%	OPTIONS(9) is set to 1 to check the user defined gradient function.
-%
-%	OPTIONS(14) is the number of samples retained from the Markov chain;
-%	default 100.
-%
-%	OPTIONS(15) is the number of samples omitted from the start of the
-%	chain; default 0.
-%
-%	OPTIONS(17) defines the momentum used when a persistent update of
-%	(leap-frog) momentum is used.  This is bounded to the interval [0,
-%	1).
-%
-%	OPTIONS(18) is the step size used in leap-frogs; default 1/trajectory
-%	length.
-%
-%	See also
-%	METROP
-%
-
-%	Copyright (c) Ian T Nabney (1996-2001)
-
-% Global variable to store state of momentum variables: set by set_state
-% Used to initialise variable if set
 
 display = options(1);
 
@@ -97,6 +22,8 @@ if options(18) > 0
 else
   step_size = 1/L;		% Default  
 end
+alpha = 0.1;
+salpha = sqrt(1-alpha^2);
 
 x = x(:)';		% Force x to be a row vector
 nparams = length(x);
@@ -107,16 +34,11 @@ samples = zeros(nsamples, nparams);	% Matrix of returned samples.
 en_save = 1;
 energies = zeros(nsamples, 1);
 
-diagn_pos = zeros(nsamples, nparams);
-diagn_mom = zeros(nsamples, nparams);
-diagn_acc = zeros(nsamples, 1);
-
 n = - nomit + 1;
-Eold = feval(f, x, data, priorPDF, varargs);	% Evaluate starting energy.
+Eold = feval(f, x, XData, yData, lambda);
 nreject = 0;
 
 p = randn(1, nparams);		% Initialise momenta at random
-lambda = 1;
 
 %%%%%%%%%%% Friction additions %%%%%%%%%%%%%%%
 Bhat = fisher;
@@ -144,57 +66,52 @@ while n <= nsamples
 
     % Choose a direction at random
     if (rand < 0.5)
-      lambda = -1;
+        direction = -1;
     else
-      lambda = 1;
+      direction = 1;
     end
-    
     % Perturb step length.
-  epsilon = lambda*step_size*(1.0 + 0.1*randn(1));
+  epsilon = direction*step_size*(1.0 + 0.1*randn(1));
 
   % First half-step of leapfrog.
-  gradfff = feval(gradf, x, data, priorPDF, abs(epsilon/2), batchSize, varargs) ...
+  % Pick a batch for stochastic gradient
+  batchInds = randi(length(yData), batchSize);
+  XBatch = XData(batchInds, :);
+  yBatch = yData(batchInds);
+  
+  gradient = feval(gradf, x, XBatch, yBatch, lambda, abs(epsilon/2), length(yData)) ...
                                 - 0.5 * epsilon * (p.* C) + ...
                                 0.5 * mvnrnd(zeroMu, 2 * incB * abs(epsilon), 1);
-    if(rem(n, 1000) == 0)
-        gradfff
-    end
-  p = p - 0.5*epsilon*gradfff;
   
+%     if(rem(n, 10000) == 0)
+%         gradient
+%     end
+  p = p - 0.5 * epsilon * gradient;
   x = x + epsilon*p;
   
   % Full leapfrog steps.
   for m = 1 : L - 1
-      gradfff = feval(gradf, x, data, priorPDF, abs(epsilon), batchSize, varargs) ...
+      gradient = feval(gradf, x, XBatch, yBatch, lambda, abs(epsilon), length(yData)) ...
                                 - epsilon * (p.*C) + ...
                                    mvnrnd(zeroMu, 2*incB * abs(epsilon), 1);
-    p = p - epsilon* gradfff;
+    p = p - epsilon* gradient;
     x = x + epsilon*p;
   end
   
   % Final half-step of leapfrog.
-    p = p - 0.5*epsilon*feval(gradf, x, data, priorPDF, abs(epsilon/2), batchSize, varargs) ...
-                                    - 0.5*epsilon * (p.*C) + ...
-                                    0.5 * mvnrnd(zeroMu, 2*incB * abs(epsilon), 1);
-
+     gradient = feval(gradf, x, XBatch, yBatch, lambda, abs(epsilon/2), length(yData)) ...
+                                - 0.5 * epsilon * (p.* C) + ...
+                                0.5 * mvnrnd(zeroMu, 2 * incB * abs(epsilon), 1);
+                            
+    p = p - 0.5*epsilon*gradient;
+    
   % Now apply Metropolis algorithm.
   Enew = feval(f, x, data, priorPDF, varargs);	% Evaluate new energy.
   p = -p;				% Negate momentum
   Hnew = Enew + 0.5*p*p';		% Evaluate new Hamiltonian.
   a = exp(Hold - Hnew);			% Acceptance threshold.
-  if (n > 0)
-    diagn_pos(n,:) = x;
-    diagn_mom(n,:) = p;
-    diagn_acc(n,:) = a;
-  end
-  
-  if (display > 1)
-    fprintf(1, 'New position is\n');
-    disp(x);
-  end
 
-  
- metropolis  = 1;
+  metropolis  = 1;
   if(~metropolis)
       Eold = Enew;			% Update energy
         if (display > 0)
--- src/Regression/stocGradLikelihood.m
+++ src/Regression/stocGradLikelihood.m
@@ -1,34 +1,38 @@
-function gradient = stocGradLikelihood(theta, data, priorPDF, stepSize, batchSize, varargs)
-    % Function to compute the stochastic gradient given the data,
+function gradient = stocGradLikelihood(theta, XBatch, yBatch, lambda, steplength, dataSize)    
+% Function to compute the stochastic gradient given the data,
     % current estimate of theta and prior for theta
     %
     % There are two options for selecting the batches:
     % Linear - linearly select the batches
     % Random - Select the batches at random
+    d = length(theta) - 1;
+    batchSize = length(yBatch);
     
-    % Asserting if theta is a row vector
-    assert(isrow(theta));
+    sigmaSq = theta(end);
+    beta = theta(1:end-1)';
+    gradient = zeros(size(theta));
     
-    batch = data(randi( size(data, 1), [batchSize, 1]), :);
-    % Evaluating the gradient
-    shifted = bsxfun(@minus, batch, theta);
-    gradient_p = (theta); 
-    gradient_L = size(data, 1)/batchSize * sum(shifted); %scale liklihood term
-    %gradient = gradient_mapping_l1(theta, gradient_L, priorPDF.lambda, stepSize);
-    gradient = gradient_p + gradient_L;
-end
+    % For the batch
+    XtX = XBatch' * XBatch;
+    Xty = XBatch' * yBatch;
 
-function g = gradient_mapping_l1(point, grad1, lambda, stepsize)
-    p = prox_l1(point - stepsize * grad1, lambda * stepsize);
-    g = (point - p) / stepsize;
+    % Gradient wrt sigmaSq
+    residual = yBatch - XBatch * beta;
+    gradient(end) = (1 + dataSize/2 + d/2) / sigmaSq - ...
+                    (dataSize / batchSize) * dot(residual, residual)/(2*sigmaSq^2)...
+                    - lambda * norm(beta, 1) / (2 * sqrt(sigmaSq)^3);
+    
+    % Gradient wrt beta 
+    % REsidual is y - xB
+    gradF = (dataSize/ batchSize) * (XtX * beta - Xty) / sigmaSq;  
+    
+    % Proximal gradient (after taking step wrt f)
+    newBeta = beta - steplength * gradF;
+    constant = steplength * lambda / sqrt(sigmaSq);
+    
+    % Computing the proximal gradient
+    proxBeta = (newBeta < -constant) .* (newBeta + constant) + ...
+                (newBeta > constant) .* (newBeta - constant);
+    
+    gradient(1:end-1) = (beta - proxBeta) / steplength;
 end
-
-function p = prox_l1(points, lambda)
-    i = points < -lambda;
-    j = points > lambda;
-    %k = abs(points) < lambda;
-    p = zeros(size(points));
-    p(i) = points(i) + lambda;
-    p(j) = points(j) - lambda;
-end
-
